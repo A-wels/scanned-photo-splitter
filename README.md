854e86551f1655456136776ff21daaf2c1dd3d93
Alex <kontakt@a-wels.de>
2023-12-06 15:02:36 +0100

    update requirements

--- README.md
+++ README.md
@@ -3,18 +3,21 @@
 Scan multiple photos at once. Afterwards split the photos with this tool.
 
 ## Installation:
+
 ```
-git clone https://codeberg.org/A-Wels/scanned-photo-splitter/
+git clone https://github.com/A-wels/scanned-photo-splitter
 cd scanned-photo-splitter
 python3 -m venv .venv
 pip install -r requirements.txt
 ```
 
 ## Usage
+
 ```
 source .venv/bin/activate
 python image_extractor.py <path>
 ```
+
 Optional:
 
 `-s` Show preview of detected photos
--- image_extractor.py
+++ image_extractor.py
@@ -14,7 +14,8 @@ FILE_EXTENSIONS: List[str] = [".jpg", ".tiff", ".png"]
 # parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument(dest='path', help="Path of the images", default=".")
-parser.add_argument('-s',help="Show detected photos", action="store_true", default=False)
+parser.add_argument('-s', help="Show detected photos",
+                    action="store_true", default=False)
 args = parser.parse_args()
 path = args.path
 
@@ -22,23 +23,25 @@ path = args.path
 # check for for filetype: image
 def check_file_extension(filename: str) -> bool:
     for e in FILE_EXTENSIONS:
-        if(filename.upper().endswith(e.upper())):
+        if (filename.upper().endswith(e.upper())):
             return True
     return False
 
+
 def show_image(image):
-        image = imutils.resize(image, height = 800)
-        cv2.imshow("Image", image)
-        print("Press any key to continue...")
-        cv2.waitKey(0)
+    image = imutils.resize(image, height=800)
+    cv2.imshow("Image", image)
+    print("Press any key to continue...")
+    cv2.waitKey(0)
+
 
 i = 0
 for file in os.listdir(path):
-    target = os.path.join(path,"processed/")
+    target = os.path.join(path, "processed/")
     if check_file_extension(file):         # check if file is an image
         if not os.path.exists(target):
             os.makedirs(target)
-        img_path = os.path.join(path,file)
+        img_path = os.path.join(path, file)
         print(img_path)
 
         # read the image
@@ -49,16 +52,16 @@ for file in os.listdir(path):
         y = image.shape[0]
         x = image.shape[1]
         if y > x:
-         image = image[20:y-40, 5:x-5]
+            image = image[20:y-40, 5:x-5]
 
         # pad the image
-        image = cv2.copyMakeBorder(image, 100,100,100,100,cv2.BORDER_CONSTANT,None,(255,255,255))
-
+        image = cv2.copyMakeBorder(
+            image, 100, 100, 100, 100, cv2.BORDER_CONSTANT, None, (255, 255, 255))
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
-        
+
         ret, th = cv2.threshold(gray, 210, 235, 1)
 
         # find contours on scanned image
@@ -66,7 +69,7 @@ for file in os.listdir(path):
             th, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
         i = 0
-        if(args.s):
+        if (args.s):
             drawing_copy = image.copy()
 
         for c in cnts:
@@ -79,17 +82,19 @@ for file in os.listdir(path):
             # filter small areas
             if area/15 < cv2.contourArea(box):
                 x, y, width, height = cv2.boundingRect(c)
-                roi = image[y:y+height, x:x+width] # cut out region of interest
-                file_write_path = os.path.join(target,str(i)+"-"+file)
+                # cut out region of interest
+                roi = image[y:y+height, x:x+width]
+                file_write_path = os.path.join(target, str(i)+"-"+file)
                 print(file_write_path)
                 try:
                     cv2.imwrite(file_write_path, roi)
-                    if(args.s):
-                        cv2.drawContours(drawing_copy, [box], -1, (0, 255, 0), 5)
+                    if (args.s):
+                        cv2.drawContours(
+                            drawing_copy, [box], -1, (0, 255, 0), 5)
                     i += 1
 
                 except:
                     print("Error writing file to " + file_write_path)
         # show preview
-        if(args.s):
+        if (args.s):
             show_image(drawing_copy)
